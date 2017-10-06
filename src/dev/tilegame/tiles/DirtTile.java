3414fde30534eb04133e8b3f5d8c15101e1c1d75
drose98 <swimmac11>
2017-10-06 14:12:18 -0400

    10-6-17 added tiles

---
+++ src/dev/tilegame/tiles/DirtTile.java
@@ -0,0 +1,12 @@
+package dev.tilegame.tiles;
+
+import dev.tilegame.gfx.Assets;
+
+public class DirtTile extends Tile {
+
+    public DirtTile(int id) {
+        super(Assets.dirt, id);
+    }
+
+
+}
---
+++ src/dev/tilegame/tiles/GrassTile.java
@@ -0,0 +1,13 @@
+package dev.tilegame.tiles;
+
+import dev.tilegame.gfx.Assets;
+
+public class GrassTile extends Tile {
+
+    public GrassTile(int id) {
+        super(Assets.grass, id);
+    }
+
+
+
+}
